46ba6df8f74de7552113bc41b54ad22c69004740
ath0111 <56038771+ath0111@users.noreply.github.com>
2020-04-29 13:19:25 -0400

    finishes list

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const person = (props) => {
-return <p>I'm a person! {props.name}, {props.age}</p>
-}
+import './Person.css';
+
+const person = ( props ) => {
+    console.log(props.children)
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            {/* props.children allows to display text between <Person> </Person> */}
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
+};
 
 export default person;
