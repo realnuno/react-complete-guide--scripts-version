8ff4a6fb21935a76cb10ed8d7a401c7df303b82b
ath0111 <56038771+ath0111@users.noreply.github.com>
2020-04-29 12:59:16 -0400

    changed css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
+  state = {
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi everyone!</h1>
+      <Person name="Max" age="28"/>
+      <Person name="Real" age="2e8"/>
+      <Person name="Maerx" age="328"/>
     </div>
   );
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = (props) => {
+return <p>I'm a person! {props.name}, {props.age}</p>
+}
+
+export default person;
